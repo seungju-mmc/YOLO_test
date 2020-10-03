ea5bcb878e02b37555fb59862f10e907da2dc8ee
seungju-mmc <ksj970213@hanyang.ac.kjr>
2020-10-03 20:20:30 +0900

    utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from PIL import ImageDraw
-
+import torch
 def img_show(data):
     image, label = data['image'], data['target']
     image_draw = ImageDraw.Draw(image)
@@ -8,3 +8,26 @@ def img_show(data):
         image_draw.rectangle([(box[0],box[1]), (box[2],box[3])], width=1)
     
     image.show()
+
+def get_optimizer(parm, network):
+    if parm['name'] == 'adam':
+        optimizer = torch.optim.Adam(network.parameters(), lr=parm['learning_rate'], weight_decay=parm['weight_decay'],eps=1e-7)
+    elif parm['name'] == 'sgd':
+        optimizer = torch.optim.SGD(network.parameters(), lr=parm['learning_rate'], weight_decay=parm['weight_decay'],
+        momentum=parm['momentum'])
+    else:
+        raise RuntimeError("optimzier error")
+    return optimizer
+
+def parallel_model(module, inp, device_ids, output_device=None):
+    if not device_ids:
+        return module(inp)
+    
+    if output_device is None:
+        output_device = device_ids[0]
+    
+    replicas =nn.parallel.replicate(module, device_ids)
+    inputs = nn.parallel.scatter(inp, device_ids)
+    replicas = replicas[:len(inputs)]
+    outputs = nn.parallel.parallel_apply(replicas, inputs)
+    return nn.parallel.gather(outputs, output_device)
