89cad5e7c8248e6657b97e43387f5ad350a85b65
seungju-mmc <ksj970213@hanyang.ac.kjr>
2020-10-03 20:30:29 +0900

    error is fixed

--- network.py
+++ network.py
@@ -31,7 +31,7 @@ class Dakrnet19(nn.Module):
         self.maxpool3 = nn.MaxPool2d(128, stride=2)
         self.conv6 = conv_batch(128,256)
         self.conv7 = conv_batch(256,128,kernel_size=1,padding=0)
-        self.conv8 = conv_bath(128,256)
+        self.conv8 = conv_batch(128,256)
         self.maxpool4 = nn.MaxPool2d(256,stride=2)
         self.conv9 = conv_batch(256,512)
         self.conv10 = conv_batch(512,256,kernel_size=1,padding=0)
@@ -45,7 +45,7 @@ class Dakrnet19(nn.Module):
         self.conv17 = conv_batch(1024,512, kernel_size=1,padding=0)
         self.conv18 = conv_batch(512,1024)
         self.conv19 = conv_batch(1024, 1000, kernel_size=1, padding=0)
-        self.avg_pool = nn.AvgPool2d()
+        self.avg_pool = nn.AvgPool2d(1)
         self.softmax = nn.Softmax()
     
     def forward(self, x):
@@ -86,7 +86,7 @@ class Darknet19_train:
     def __init__(self, batch_size=128, epoch=10, lr=1e-1, weight_decay = 5e-4, momentum=0.9, device="cpu", division=1,burn_in = True):
         self.epoch = epoch
         self.batch_size = batch_size
-        self.mini_batch = int(self.batch_size/divicion)
+        self.mini_batch = int(self.batch_size/division)
         self.device = torch.device(device)
         self.division = division
         self.dataset = DataLoader(ImageNetDataset(), batch_size=self.mini_batch,shuffle=True,num_workers=-1)
@@ -106,7 +106,7 @@ class Darknet19_train:
         self.critieron = nn.CrossEntropyLoss()
 
     def lr_scheduling(self, step):
-        if stpe<1001 and self.burn_in:
+        if step<1001 and self.burn_in:
             lr = self.lr *(step/1004)**4
             for g in self.optimizer.param_groups:
                 g['lr'] = lr
@@ -118,7 +118,7 @@ class Darknet19_train:
             Loss = []
 
             n = 0
-            for data in self.self.dataset:
+            for data in self.dataset:
                 image, label = data[0].to(self.device), data[1].to(self.device)
                 # hypo = parallel_model(self.network, image, output_device = 3, device_ids=[0,1,2,3])
                 hypo = self.network.forward(image)
@@ -144,5 +144,5 @@ class Darknet19_train:
                         Loss = []
                         
 if __name__ == "__main__":
-    darknet19 = Dakrnet19_train(batch_size=64,device="cuda:3")
+    darknet19 = Darknet19_train(batch_size=64,device="cuda:3")
     darknet19.run()
