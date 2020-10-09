5e1fb06d8627d42750113fa6a2278de43c9eb029
seungju-mmc <ksj970213@hanyang.ac.kjr>
2020-10-09 23:40:00 +0900

    .

--- Download_data.py
+++ Download_data.py
@@ -1,6 +1,7 @@
 import os
 import wget
 import tarfile
+import zipfile
 
 
 def download_VOC_2012():
@@ -31,6 +32,53 @@ def download_VOC_2012():
                 print('Voc 2012 is unzip')
                 tf.close()
     
-    
+
+def download_COCO_2017_Val():
+    url = 'http://images.cocodataset.org/zips/val2017.zip'
+    folder = './MS_COCO_2017'
+
+    os.mkdir(folder)
+    wget.download(url, folder + '/')
+    print('Download is completed')
+
+    file_list = os.listdir(folder)
+    if file_list == []:
+        raise RuntimeError('Download might be failed.')
+
+    for i in range(len(file_list)):
+        element = file_list[i]
+        if '.zip' in element:
+            file_name = element
+            with zipfile.ZipFile(folder + '/' + file_name, 'r') as tf:
+                tf.extractall(folder + '/')
+                print('COCO 2017 val is unzip')
+                tf.close()
+
+                
+def download_COCO_2017_anno():
+    url = 'http://images.cocodataset.org/annotations/annotations_trainval2017.zip'
+    folder = './MS_COCO_2017_Anno'
+    if os.path.exists(folder):
+        print('MS COCO 2017_Anno was already downloaded')
+    else:
+        os.mkdir(folder)
+        wget.download(url, folder + '/')
+        print('Download is completed')
+    file_list = os.listdir(folder)
+    if file_list == []:
+        raise RuntimeError('Download might be failed.')
+
+    for i in range(len(file_list)):
+        element = file_list[i]
+        if '.zip' in element:
+            file_name = element
+            with zipfile.ZipFile(folder + '/' + file_name, 'r') as tf:
+                tf.extractall(folder + '/')
+                print('COCO 2017_anno is unzip')
+                tf.close()
+
+
 if __name__ == "__main__":
-    download_VOC_2012()
+    # download_VOC_2012()
+    download_COCO_2017_Val()
+    download_COCO_2017_anno()
